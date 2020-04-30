0f18f6d54d9fff93286f671ec0cd1d4e7e27ff03
Kushagra Singh <kushagra.cse.11498@gmail.com>
2020-04-30 20:24:49 +0530

    Update Largest Coprime Divisor.cpp

--- 2.Math/Largest Coprime Divisor.cpp
+++ 2.Math/Largest Coprime Divisor.cpp
@@ -48,3 +48,22 @@ int Solution::cpFact(int A, int B) {
         }
         return max;
 }
+
+
+_____________________________________________________________________________________________________________________________________________
+
+
+Another Solotion
+
+int gcd(int A, int B){
+    if(A==0) return B;
+    if(B==0) return A;
+    if(A<B) swap(A, B);
+    return gcd(A%B, B);
+}
+int Solution::cpFact(int A, int B) {
+    for(int g=gcd(A, B); g>1; g=gcd(A, g)) A/=g;
+    return A;
+}
+
+
