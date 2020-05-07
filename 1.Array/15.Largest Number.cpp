0092324b35f7f8cbc1b0f66f22aff94b13966ed9
Kushagra Singh <kushagra.cse.11498@gmail.com>
2020-05-07 12:59:32 +0530

    Update 15.Largest Number.cpp

--- 1.Array/15.Largest Number.cpp
+++ 1.Array/15.Largest Number.cpp
@@ -8,39 +8,78 @@ Given [3, 30, 34, 5, 9], the largest formed number is 9534330.
 
 Note: The result may be very large, so you need to return a string instead of an integer.
 
+ 
+ ____________________________________________________________________________________________________________________________________________
+
+
+int myCompare(string X, string Y)
+{
+    // first append Y at the end of X
+    string XY = X.append(Y);
+ 
+    // then append X at the end of Y
+    string YX = Y.append(X);
+ 
+    // Now see which of the two formed numbers is greater
+    return XY.compare(YX) > 0 ? 1: 0;
+}
+string Solution::largestNumber(const vector<int> &A) {
+    vector<string> b;
+    for(int i=0;i<A.size();i++){
+        b.push_back(to_string(A[i]));
+    }
+    sort(b.begin(),b.end(),myCompare);
+    string ans="";
+    for(int i=0;i<b.size();i++){
+     ans+=b[i];
+    }
+    int i=0;
+    while(ans[i]=='0'){
+        i++;
+    }
+    if(i==ans.length())
+     ans="0";
+    return ans;
+}
+
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
- bool compare(string a , string b){
-        return (a+b>b+a);
-    }
-    
-    
-int all_zero(const vector<int> &A){
-        for(int i=0;i<A.size();i++){
-            if(A[i]!=0){
-                return 0;
-            }
-        }
-        return 1;
-    }
+bool cmp(string a, string b)
+{
+    return (a+b >  b+a);
+}
+
 string Solution::largestNumber(const vector<int> &A) {
+    
+    vector <string> v;
     int n = A.size();
-    if(all_zero(A)){
-        return "0";
-    }
+    
+    int flag=0;
     
     
-    vector<string> digit;
+    for(int i=0;i<n;i++)
+      {
+          v.push_back(to_string(A[i]));
+          if(A[i] >0)
+          flag =1;
+      }
+      
+      
+      if(!flag)
+       return "0";
+       
+       
+        
+    sort(v.begin() , v.end(),cmp);
     
+    string s ="";
     
-    for(int i =0;i<n;i++){
-        digit.push_back(to_string(A[i]));
-    }
-    sort(digit.begin(),digit.end(),compare);
-    string s="";
-    for(int i=0;i<n;i++){
-        s=s+digit[i];
-    }
-    return s;
+    for(int i =0;i<n;i++)
+          s = s+v[i];
+     
+     
+     
+     
+     return s;
 }
