58ef81c9b6bc9973539a80ae7542c1a77a9021ef
Kushagra Singh <kushagra.cse.11498@gmail.com>
2020-05-17 17:18:06 +0530

    Create 04. Amazing Subarrays.cpp

---
+++ 4.String/04. Amazing Subarrays.cpp
@@ -0,0 +1,55 @@
+
+
+You are given a string S, and you have to find all the amazing substrings of S.
+
+Amazing Substring is one that starts with a vowel (a, e, i, o, u, A, E, I, O, U).
+
+Input
+
+Only argument given is string S.
+
+Output
+
+Return a single integer X mod 10003, here X is number of Amazing Substrings in given string.
+
+Constraints
+
+1 <= length(S) <= 1e6
+S can have special characters
+
+Example
+
+Input
+    ABEC
+
+Output
+    6
+
+Explanation
+	Amazing substrings of given string are :
+	1. A
+	2. AB
+	3. ABE
+	4. ABEC
+	5. E
+	6. EC
+	here number of substrings are 6 and 6 % 10003 = 6.
+
+___________________________________________________________________________________________________________________________________________________
+
+
+int Solution::solve(string A) {
+    
+    int ans=0;
+    int n = A.length();
+    
+    for(int i=0;i<n;i++)
+    {
+        if(A[i]=='A'||A[i]=='E'||A[i]=='I'||A[i]=='O'||A[i]=='U'||A[i]=='a'||A[i]=='e'||A[i]=='i'||A[i]=='o'||A[i]=='u')
+        {
+            ans = (ans + (n - i)) % 10003;
+        }
+    }
+    
+    return ans%10003;
+}
