040d99ced9f0d8823a5dd4d2a56c96838acc0deb
Kushagra Singh <kushagra.cse.11498@gmail.com>
2020-06-15 17:50:50 +0530

    Update 03.Capture Regions on Board.cpp

--- 15.Graph Data Structure/03.Capture Regions on Board.cpp
+++ 15.Graph Data Structure/03.Capture Regions on Board.cpp
@@ -32,76 +32,21 @@ Output 1:
 Explanation:
     O in (4,2) is not surrounded by X from below.
 
-
-________________________________________________________________________________________________________________________________________________________________________________________________________________________
-
-
-bool memo[1002][1002];
-vector<vector<char> >v;
-int n,c;
-void dfs(int i,int j){
-    if(memo[i][j])return;
-    if(i>=n||i<0||j<0||j>=c)return;
-    if(v[i][j]=='X')return;
-    memo[i][j]=true;
-    dfs(i+1,j);
-    dfs(i,j+1);
-    dfs(i-1,j);
-    dfs(i,j-1);
-}
-void Solution::solve(vector<vector<char> > &A) {
-    memset(memo,0,sizeof(memo));
-    n=A.size();
-    c=A[0].size();
-    v=A;
-    for(int i=0;i<c;i++){
-        if(A[0][i]!='X')dfs(0,i);
-        if(A[n-1][i]!='X')dfs(n-1,i);
-    }
-    for(int i=0;i<n;i++){
-        if(A[i][0]!='X')dfs(i,0);
-        if(A[i][c-1]!='X')dfs(i,c-1);
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<c;j++){
-            if(!memo[i][j])A[i][j]='X';
-        }
-    }
-}
 _______________________________________________________________________________________________________________________________________________________________________________________________
 
-
--------------------------------------------------Partially Accepted-----------------------------------------------------------------------
-
-
-int isSafe(vector <vector <char> > A , int i , int j,vector <vector <int> > visited)
-{
-    int n = A.size();
-    int m = A[0].size();
-    
-    if(i <0 || j<0 || i>=n || j>=m || A[i][j] == 'X'|| visited[i][j]==1)
-    return 0;
-    
-    
-    return 1;
-}
 void dfs(vector <vector <char> > &A , int i ,int j,vector <vector <int> > &visited)
 {
     
-
+if(i <0 || j<0 || i>=A.size() || j>=A[0].size() || A[i][j] == 'X'|| visited[i][j]==1)
+    return ;
+    
     A[i][j]='O';
     visited[i][j]=1;
 
-    if(isSafe(A,i+1,j,visited))
+
      dfs(A,i+1,j,visited);
-    
-    if(isSafe(A,i-1,j,visited))
      dfs(A,i-1,j,visited);
-    
-   if(isSafe(A,i,j+1,visited))
-    dfs(A,i,j+1,visited);
-    
-   if(isSafe(A,i,j-1,visited))
+     dfs(A,i,j+1,visited);
      dfs(A,i,j-1,visited);
     
 }
@@ -140,4 +85,40 @@ void Solution::solve(vector<vector<char> > &A) {
           if(A[i][j]=='-')
             A[i][j]='X';
     
+}
+
+________________________________________________________________________________________________________________________________________________________________________________________________________________________
+
+
+bool memo[1002][1002];
+vector<vector<char> >v;
+int n,c;
+void dfs(int i,int j){
+    if(memo[i][j])return;
+    if(i>=n||i<0||j<0||j>=c)return;
+    if(v[i][j]=='X')return;
+    memo[i][j]=true;
+    dfs(i+1,j);
+    dfs(i,j+1);
+    dfs(i-1,j);
+    dfs(i,j-1);
+}
+void Solution::solve(vector<vector<char> > &A) {
+    memset(memo,0,sizeof(memo));
+    n=A.size();
+    c=A[0].size();
+    v=A;
+    for(int i=0;i<c;i++){
+        if(A[0][i]!='X')dfs(0,i);
+        if(A[n-1][i]!='X')dfs(n-1,i);
+    }
+    for(int i=0;i<n;i++){
+        if(A[i][0]!='X')dfs(i,0);
+        if(A[i][c-1]!='X')dfs(i,c-1);
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<c;j++){
+            if(!memo[i][j])A[i][j]='X';
+        }
+    }
 }
