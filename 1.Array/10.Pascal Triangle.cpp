dbf11efc938038169031bca9e5a35e8439db147f
Kushagra Singh <kushagra.cse.11498@gmail.com>
2022-02-02 23:31:37 +0530

    Update 10.Pascal Triangle.cpp

--- 1.Array/10.Pascal Triangle.cpp
+++ 1.Array/10.Pascal Triangle.cpp
@@ -61,3 +61,27 @@ vector<vector<int> > Solution::solve(int A) {
     }
     return pas;
 }
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Another solution :
+     vector<vector<int> > Solution::solve(int A) {
+    vector<vector<int>> ans;
+    
+    if(A==0){
+        return ans;
+    }
+    for(int size=1;size<=A;size++){
+        vector<int> v(size , 1);
+        
+        if(size>=3){
+            for(int i = 1;i<size-1;i++){
+                v[i] = ans[size-2][i-1]+ans[size-2][i];
+            }
+        }
+        
+        ans.push_back(v);
+    }
+    return ans;
+}
+
