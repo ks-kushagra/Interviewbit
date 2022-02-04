faa13e452cc1b1d167906fcfb57ae4e117e66554
Kushagra Singh <kushagra.cse.11498@gmail.com>
2022-02-04 14:22:28 +0530

    Update 11.Kth Row of Pascal's Triangle.cpp

--- 1.Array/11.Kth Row of Pascal's Triangle.cpp
+++ 1.Array/11.Kth Row of Pascal's Triangle.cpp
@@ -39,3 +39,17 @@ vector<int> Solution::getRow(int A) {
     }
     return k;
 }
+
+
+--------------Another -----------------------------------
+    vector<int> Solution::getRow(int A) {
+    
+       vector<int>temp;
+    
+        int n  = 1;
+        for( int col = 0; col <= A ; col++){// nth row will have n + 1 elements in pascal triangle
+            temp.push_back(n);
+            n = n * (A - col)/(col + 1);
+        }
+    return temp; 
+}
