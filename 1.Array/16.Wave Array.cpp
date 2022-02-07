90160a40c1ad4b429f00aee6ef72ad45ff7a84e5
Kushagra Singh <kushagra.cse.11498@gmail.com>
2022-02-07 20:00:21 +0530

    Update 16.Wave Array.cpp

--- 1.Array/16.Wave Array.cpp
+++ 1.Array/16.Wave Array.cpp
@@ -13,28 +13,22 @@ Another possible answer : [4, 1, 3, 2]
     NOTE : If there are multiple answers possible, return the one thats lexicographically smallest.
     So, in example case, you will return [2, 1, 4, 3] 
 
-
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 vector<int> Solution::wave(vector<int> &A) {
+    int n = A.size();
     
-    sort(A.begin(),A.end());
-    
-    for(int i=1;i<A.size();i +=2)
-    {
-        int t = A[i];
-        A[i]=A[i-1];
-        A[i-1]=t;
+    if(n<=1){
+        return A;
     }
-    
+    sort(A.begin(),A.end());
+    int index=0;
+    while(index < n-1){
+        swap(A[index], A[index+1]);
+        index = index+2;
+    }
     return A;
 }
-
-
-
-
-
-
 __________________________________________________________________________________________________________________________________________________________________________________________
 vector<int> Solution::wave(vector<int> &A) {
     vector<int> a;
