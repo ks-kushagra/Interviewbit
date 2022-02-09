36e4d1bac9742104f24f65640024c6430219ced6
Kushagra Singh <kushagra.cse.11498@gmail.com>
2022-02-09 16:01:48 +0530

    Update 13.Noble Integer.cpp

--- 1.Array/13.Noble Integer.cpp
+++ 1.Array/13.Noble Integer.cpp
@@ -1,5 +1,19 @@
 Given an integer array, find if an integer p exists in the array such that the number of integers greater than p in the array equals to p
 If such an integer is found return 1 else return -1.
+    
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+int Solution::solve(vector<int> &A) {
+    sort(A.begin(),A.end());
+    
+    for(auto it = A.begin();it != A.end();it++){
+        auto up = upper_bound(A.begin() , A.end() , *it);
+        if(*it == distance(up , A.end())){
+            return 1; 
+        }
+    }
+    return -1;
+}
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
